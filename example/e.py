cdb32a3f6196e01b20b47bf4194af24a0fa43b7d
unknown <ah4904951@gmail.com>
2023-11-28 11:38:11 +0200

    first commit

---
+++ example/e.py
@@ -0,0 +1,17 @@
+x="fkdjgdr\
+dfgd,fg\"\n\
+dlfkgldfg\\n"
+
+x="""
+sdfmlkg
+srglkmlrg"""
+if x()
+    print("sdas")
+    if x
+        print("dscsc")
+    return
+x
+
+print()
+name = "Alice" # This is a string variable that stores the name Alice
+print("Hello, " + name)
